9fa3758041552ec4b3be1ebff6e799ef0675fd56
Marina8989 <marina.mill209@gmail.com>
2021-05-27 18:44:10 -0700

    react

--- src/App.js
+++ src/App.js
@@ -3,47 +3,76 @@ import './index.css';
 import Images from './components/Images';
 
 
-class App extends React.Component{
-  constructor(props) {
-    console.log('App Contructor');
-        super(props);
-        this.state = {
-          title: 'Hello React 2',
-          isShowing: false
-        }
-        this.handleClick = this.handleClick.bind(this);
-  }
+function App() {
+   
+  const [title, setTitle] = React.useState('Hello React');
+  const [isShowing, setIsShowing] = React.useState(false);
 
-   componentDidMount() {
-     console.log('App Mounted');
-     this.setState({title: 'Hello Lifecycle State'});
-   }
-
-
-
-
-  // states are immutable
- handleClick() {
-   this.setState((prevState) => {
-     return {
-       isShowing: !prevState.isShowing
-     }
-   })
+  function handleClick() {
+  setIsShowing(!isShowing);
  }
 
-  render() {
-    console.log('App Render');
-    return (
-      <div style={{textAlign: 'center'}}>
-         <div style={{marginTop: '1rem'}}>{this.state.title}</div>
+
+  return (
+    <div style={{textAlign: 'center'}}>
+         <div style={{marginTop: '1rem'}}>{title}</div>
          <div>
-           <button onClick={this.handleClick} className="button" style={{padding: '10px', backgroundColor: 'lightblue', color: 'white'}}>Toggle Image</button>
-            {this.state.isShowing ? <Images /> : null}
+           <button onClick={handleClick} className="button" style={{padding: '10px', backgroundColor: 'lightblue', color: 'white'}}>Toggle Image</button>
+            {isShowing ? <Images /> : null}
          </div>
       </div>
-    )
-  }
+  )
 }
+
+
+
+
+
+
+
+//class App extends React.Component{
+  // constructor(props) {
+  //   console.log('App Contructor');
+  //       super(props);
+  //       this.state = {
+  //         title: 'Hello React 2',
+  //         isShowing: false
+  //       }
+  //       this.handleClick = this.handleClick.bind(this);
+  // }
+
+//    componentDidMount() {
+//      console.log('App Mounted');
+//      this.setState({title: 'Hello Lifecycle State'});
+//    }
+
+//    componentDidUpdate() {
+//      console.log('App Updated');
+//    }
+
+
+//   // states are immutable
+//  handleClick() {
+//    this.setState((prevState) => {
+//      return {
+//        isShowing: !prevState.isShowing
+//      }
+//    })
+//  }
+
+//   render() {
+//     console.log('App Render');
+//     return (
+//       <div style={{textAlign: 'center'}}>
+//          <div style={{marginTop: '1rem'}}>{this.state.title}</div>
+//          <div>
+//            <button onClick={this.handleClick} className="button" style={{padding: '10px', backgroundColor: 'lightblue', color: 'white'}}>Toggle Image</button>
+//             {this.state.isShowing ? <Images /> : null}
+//          </div>
+//       </div>
+//     )
+//   }
+// }
 
 
 
